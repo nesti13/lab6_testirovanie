dc0202020f3e01fefb6a74f1ce509d4db7ce8594
Jana <90056190+nesti13@users.noreply.github.com>
2022-11-22 18:31:21 +0300

    Add files via upload

---
+++ Collections (RU)/OOP/OOP/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+     class Car
+    {
+        public string _Type;
+        public Yana _yana;
+        public Chassis _chassis;
+        public Transmission _transmission;
+
+        public Car()
+        {
+
+        }
+        public Car(string type,Yana yana, Chassis chassis, Transmission transmission)
+        {
+            _Type = type;
+            _yana = yana;
+            _chassis = chassis;
+            _transmission = transmission;
+
+        }
+        public void Print()
+        {
+            Console.WriteLine("---------------------------------------------------------------------");
+            Console.WriteLine($"Тип: {_Type}, Хар-ки:");
+            _yana.Print();
+            _chassis.Print();
+            _transmission.Print();
+            Console.WriteLine("---------------------------------------------------------------------");
+        }
+    }
+}
---
+++ Collections (RU)/OOP/OOP/Transmission.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    public class Transmission
+    {
+        public int _Count_of_speed;
+        public string _Creater;
+        public string _Type;
+        
+        public Transmission()
+        {
+
+        }
+        public Transmission(int count, string creater, string type)
+        {
+            _Count_of_speed = count;
+            _Creater = creater;
+            _Type = type;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Трансмиссия");
+            Console.WriteLine("Count_of_speed, Creater,Type");
+            Console.WriteLine($"{_Count_of_speed}, {_Creater},{_Type}");
+
+        }
+    }
+}
